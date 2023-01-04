471255590647a281b1aa8a5c7bcf6ca5ce82590d
Dmitriy Lomanchuk <dmitriy@skillmac.local>
2023-01-04 18:03:13 +0300

    Add initial code

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/daloman/ldapping-exporter
+
+go 1.19
+
+require (
+	github.com/Azure/go-ntlmssp v0.0.0-20220621081337-cb9428e4ac1e // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.4 // indirect
+	github.com/go-ldap/ldap/v3 v3.4.4 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/mikioh/tcp v0.0.0-20190314235350-803a9b46060c // indirect
+	github.com/mikioh/tcpinfo v0.0.0-20190314235526-30a79bb1804b // indirect
+	github.com/mikioh/tcpopt v0.0.0-20190314235656-172688c1accc // indirect
+	github.com/prometheus/client_golang v1.14.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
+
+	ldap "github.com/go-ldap/ldap/v3"
+)
+
+var bindUser, bindPassword string
+var ldapAddr string
+var ldapPort string
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+}
+func main() {
+	bindUser, bindUserDefined := os.LookupEnv("BIND_USER")
+	if bindUserDefined == false || bindUser == "" {
+		log.Fatal("BIND_USER variable is undefined or is empty")
+	}
+
+	bindPassword, bindPasswordDefined := os.LookupEnv("BIND_PASSWORD")
+	if bindPasswordDefined == false || bindPassword == "" {
+		log.Fatal("BIND_PASSWORD variable is undefined or is empty")
+	}
+
+	ldapAddr, ldapAddrDefined := os.LookupEnv("LDAP_ADDR")
+	if ldapAddrDefined == false || ldapAddr == "" {
+		log.Fatal("LDAP_ADDR variable is undefined or is empty")
+	}
+
+	ldapPort, ldapPortDefined := os.LookupEnv("LDAP_PORT")
+	if ldapPortDefined == false || ldapPort == "" {
+		log.Fatal("LDAP_PORT variable is undefined or is empty")
+	}
+
+	ldapUrl := "ldap://" + ldapAddr + ":" + ldapPort
+	recordMetrics(ldapUrl, bindUser, bindPassword)
+	//
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
+
+}
+
+func recordMetrics(ldapUrl, bindUser, bindPassword string) {
+
+	go func() {
+		for {
+			bindLdapDuration, searchTimeDuration := probeLdap(ldapUrl, bindUser, bindPassword)
+			bindDuration.Set(bindLdapDuration)
+			searchDuration.Set(searchTimeDuration)
+			time.Sleep(10 * time.Second)
+		}
+	}()
+}
+
+var (
+	bindDuration = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "ldap_bind_delay",
+		Help: "LDAP bind delay milliseconds",
+	})
+	searchDuration = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "ldap_search_delay",
+		Help: "LDAP search request delay milliseconds",
+	})
+)
+
+func timeTrace(name string) float64 {
+	start := time.Now()
+	return float64(time.Since(start))
+}
+
+func probeLdap(ldapUrl, bindUser, bindPassword string) (float64, float64) {
+	l, err := ldap.DialURL(ldapUrl)
+	if err != nil {
+		log.Fatalf("Connection failure: %v\n", err)
+	}
+
+	defer l.Close()
+
+	startBindTime := time.Now()
+
+	err = l.Bind(bindUser, bindPassword)
+	if err != nil {
+		log.Fatalf("Bind failure: %v\n", err)
+	}
+
+	bindLdapDuration := float64(time.Since(startBindTime).Milliseconds())
+	log.Infof("Bind duration: %v", bindLdapDuration)
+
+	searchRequest := ldap.NewSearchRequest(
+		"DC=example,DC=com", // The base dn to search
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		"(&(objectclass=*))", // The filter to apply
+		[]string{"dn", "cn"}, // A list attributes to retrieve
+		nil,
+	)
+
+	startSearchTime := time.Now()
+	sr, err := l.Search(searchRequest)
+	if err != nil {
+		log.Fatalf("Search failure: %v\n", err)
+	}
+
+	searchTimeDuration := float64(time.Since(startSearchTime).Milliseconds())
+	log.Infof("Search duration: %v", searchTimeDuration)
+	for _, entry := range sr.Entries {
+		log.Infof("%s: %v\n", entry.DN, entry.GetAttributeValue("cn"))
+	}
+
+	return bindLdapDuration, searchTimeDuration
+}
